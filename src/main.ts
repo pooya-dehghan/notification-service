924d89626a0419614fc5eeb70789a1f1a0773b9c
pooya_dehghan <pooyadehghan@yahoo.com>
2023-04-27 14:23:17 +0430

    exports and debugging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('NotificationService')
+    .setDescription('The notification service API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -4,6 +4,7 @@ import { PrismaController } from './prisma.controller';
 
 @Module({
   controllers: [PrismaController],
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports : [PrismaService]
 })
 export class PrismaModule {}
